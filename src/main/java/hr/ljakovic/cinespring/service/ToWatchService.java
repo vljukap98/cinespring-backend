3b467f01adc9045a4847300706844998b43903ad
Luka Jaković <30662155+vljukap98@users.noreply.github.com>
2022-08-08 11:20:55 +0200

    Initialize services for new entities

---
+++ src/main/java/hr/ljakovic/cinespring/service/ToWatchService.java
@@ -0,0 +1,21 @@
+package hr.ljakovic.cinespring.service;
+
+import hr.ljakovic.cinespring.repo.AppUserRepo;
+import hr.ljakovic.cinespring.repo.FavoritesRepo;
+import hr.ljakovic.cinespring.util.TmdbApiUtils;
+import info.movito.themoviedbapi.TmdbApi;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToWatchService {
+
+    private final TmdbApi tmdbApi;
+    private final AppUserRepo appUserRepo;
+    private final FavoritesRepo favoritesRepo;
+
+    public ToWatchService(AppUserRepo appUserRepo, FavoritesRepo favoritesRepo) {
+        this.tmdbApi = new TmdbApi(TmdbApiUtils.API_KEY);
+        this.appUserRepo = appUserRepo;
+        this.favoritesRepo = favoritesRepo;
+    }
+}
