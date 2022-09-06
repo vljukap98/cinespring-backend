3c0338b4d385ca20c6c72d97589611b76cd638e7
Luka Jaković <30662155+vljukap98@users.noreply.github.com>
2022-09-06 12:39:55 +0200

    allow swagger, remove hibernate in logs

--- src/main/java/hr/ljakovic/cinespring/conf/WebConf.java
+++ src/main/java/hr/ljakovic/cinespring/conf/WebConf.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,6 +36,16 @@ public class WebConf extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(cineSpringUserDetailsService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 
     @Override
